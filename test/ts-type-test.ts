5086b69b308f4807168aadb16207b69fd9777c44
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-02-01 14:06:31 +0800

    automate checking in ts-type-test.ts

--- test/ts-type-test.ts
+++ test/ts-type-test.ts
@@ -1,16 +1,55 @@
-import { genTsType } from '../src/ts-type';
+import { genTsType, genTsTypeOptions } from '../src/ts-type';
 
-console.log(
-  genTsType(
-    { user: 'Alice', friends: [{ user: 'Bob', since: new Date() }] },
-    { format: true },
-  ),
+function test(name: string, expectedType: string, data: any, options?: genTsTypeOptions) {
+  let generatedType = genTsType(data, options);
+  if (generatedType !== expectedType) {
+    console.error('failed:', name);
+    console.error('ground truth:', expectedType);
+    console.error('generated type:', generatedType);
+    console.error('data:', data);
+    process.exit(1);
+  }
+  console.log('passed:', name);
+}
+
+test(
+  'nested Object and Array with formatting',
+  `{
+  user: string;
+  friends: Array<{
+    user: string;
+    since: Date;
+  }>;
+}`,
+  { user: 'Alice', friends: [{ user: 'Bob', since: new Date() }] },
+  { format: true },
 );
-console.log(
-  genTsType([{ name: 'Alice' }, { name: 'Bob', nickname: 'Charlie' }], {
-    allowOptionalFieldInArray: true,
-  }),
+
+test(
+  'Array of Object with optional field',
+  `Array<{
+  name: string;
+  nickname?: string;
+}>`,
+  [{ name: 'Alice' }, { name: 'Bob', nickname: 'Charlie' }],
+  { allowOptionalFieldInArray: true },
 );
-console.log(genTsType(new Set([1, 2])));
-console.log(genTsType(new Set([1, 2, 'str']), { allowMultiTypedArray: true }));
-console.log(genTsType(new Map([['age', 12]])));
+
+test(
+  'Set',
+  `Set<number>`,
+  new Set([1, 2]),
+);
+
+test(
+  'Set with multiple type',
+  `Set<number | string>`,
+  new Set([1, 2, 'str']),
+  { allowMultiTypedArray: true },
+);
+
+test(
+  'Map',
+  `Map<string, number>`,
+  new Map([['age', 12]]),
+);
